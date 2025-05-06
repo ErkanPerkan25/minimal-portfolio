a77c62ebf74e98078c16bfb87f3fd2c98cbebc1e
Eric Hansson <erichansson.united@gmail.com>
2025-05-05 22:02:03 -0500

    Added correct url to the projects

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -9,13 +9,13 @@ export default [
         title: "Minimal Portfolia Website",
         imgUrl: "assets/portfolio.jpg",
         stack: ["React", "Tailwindcss", "Vite", "JavaScript"],
-        link: "https://github.com/Lindenwood-Performance-Science/ROAR",
+        link: "https://github.com/ErkanPerkan25/minimal-portfolio",
     },
     { 
         title: "Simple Web Server",
         imgUrl: "assets/web_server.jpg",
         stack: ["C++"],
-        link: "",
+        link: "https://github.com/ErkanPerkan25/simple_web_server",
     },
     {
         title: "LogiQuest",
