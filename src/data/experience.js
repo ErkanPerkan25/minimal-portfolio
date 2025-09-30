69a2b323359aed2934b9ccb11ec4b18efcbb84ad
Eric <erichansson.united@gmail.com>
2025-09-30 14:42:35 -0400

    Added information

--- src/data/experience.js
+++ src/data/experience.js
@@ -3,22 +3,21 @@ export default [
         title:"Project Leader",
         company:"Lindenwood Men's Baseball",
         desc: [
-            ` Engineered and lead a full-stack web analytics platforms that deliver real-time pitching and
-            batting KPIs, directly elevating training effectiveness and in-game decision-making.`, 
-            `Produced advanced sabermetric reports for coaches and athletes, enabling data-backed performance evaluations and lineup optimizations.`,
-            `Partnered with strength & conditioning staff and sports-science faculty to design data-driven athlete development programs.`,
-            `Led data science camps, mentoring high-school students and promoting STEM careers, which increased student interest and participation in STEM fields.`
+            `I collaborated, engineered, and led a full‑stack web analytics platform that delivered real‑time pitching and batting KPIs and higher team performance.`, 
+            `Designed REST APIs in R and Python with secure data handling to handle backend requests and database queries.`,
+            `Managed version control with Git branching workflows and basic CI build checks for reliable developments.`,
+            `Mentored high-school students in data science camps which increased student interest and participation in STEM fields.`
         ],
         location: "Saint Charles, MO",
         time: "Jan. 2024 - May 2025",
     },
     {
         title:"IT-Technican",
-        company:"AvataraCloud",
+        company:"Avatara",
         desc:[
-            `Delivered Tier-1/2 support for 80+ enterprise clients, provisioning accounts, deploying software, and resolving incidents via live chat.`,
+            `Delivered Tier‑1/2 support for 80+ enterprise clients, provisioning, deploying software, and resolving incidents via live chat.`,
             `Executed large-scale VM migrations and company-wide software upgrades with zero unplanned downtime.`,
-            `Resolved proactive technical issues and troubleshooting guides, cutting average ticket resolution time by 20 %.`
+            `Resolved proactive technical issues and troubleshooting guides, cutting average ticket resolution time by 20%.`
         ],
         location: "St.Louis, MO",
         time: "Oct. 2022 - Dec. 2023",
